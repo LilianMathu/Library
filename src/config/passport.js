dd29e44cf6698de6736fdd87c7d31c407978f818
lilianmathu <lilianmathu2018@gmail.com>
2019-04-29 12:48:35 +0300

    user authentication

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,5 +1,5 @@
 const passport = require('passport');
-require('./strategies/local.strategy');
+require('./strategies/local.strategy')();
 
 module.exports = function passportConfig(app) {
   app.use(passport.initialize());
@@ -7,14 +7,11 @@ module.exports = function passportConfig(app) {
 
   // stores users in session
   passport.serializeUser((user, done) => {
-    done(null, user.id);
+    done(null, user);
   });
 
-  // retrieve user form sessionrequire('./strategies/local.strategy');
-
+  // retrieve user from session
   passport.deserializeUser((user, done) => {
-    done(null, user.id);
+    done(null, user);
   });
-
-
 };
